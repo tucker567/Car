ee82b3fe2911c1110f1e32fe6b4b22960f149369
tucker567 <tuckerstevens08@gmail.com>
2025-10-07 23:09:35 -0600

    Revert "Update TestMap scene layout and UI elements"
    This reverts commit 7af47df38dac421a953716db998f52080443b5bc.

--- Car/Assets/Scripts/MissionWaypoint.cs
+++ Car/Assets/Scripts/MissionWaypoint.cs
@@ -1,42 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using System.Collections.Generic;
 
 public class MissionWaypoint : MonoBehaviour
 {
     public Image waypointImage;
+    public Transform target;
     public TextMeshProUGUI meter;
     public Vector3 RectOffset;
-    public List<Transform> targets; // Array of targets
-    public float activationDistance = 10f; // Distance to switch to next target
-    public Transform carTransform; // Reference to the car
-
-    private int currentTargetIndex = 0;
 
     void Update()
     {
-        if (targets == null || targets.Count == 0 || currentTargetIndex >= targets.Count)
-        {
-            waypointImage.enabled = false;
-            meter.text = "";
-            return;
-        }
-
-        Transform currentTarget = targets[currentTargetIndex];
-
         float minX = waypointImage.GetPixelAdjustedRect().width / 2;
         float maxX = Screen.width - minX;
 
         float minY = waypointImage.GetPixelAdjustedRect().height / 2;
         float maxY = Screen.height - minY;
 
-        Vector2 pos = UnityEngine.Camera.main.WorldToScreenPoint(currentTarget.position + RectOffset);
+        Vector2 pos = UnityEngine.Camera.main.WorldToScreenPoint(target.position + RectOffset);
 
-        Transform camTransform = UnityEngine.Camera.main.transform;
-
-        if (Vector3.Dot((currentTarget.position - camTransform.position), camTransform.forward) < 0)
+        if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
         {
+            // Target is behind the camera, clamp to nearest edge
             if (pos.x < Screen.width / 2)
                 pos.x = maxX;
             else
@@ -48,25 +33,7 @@ public class MissionWaypoint : MonoBehaviour
 
         waypointImage.transform.position = pos;
 
-        // Use carTransform for distance
-        float distance = Vector3.Distance(currentTarget.position, carTransform.position);
+        float distance = Vector3.Distance(target.position, transform.position);
         meter.text = ((int)distance).ToString() + "m";
-
-        // Check if close enough to switch to next target
-        if (distance < activationDistance)
-        {
-            currentTarget.gameObject.SetActive(false);
-
-            currentTargetIndex++;
-            if (currentTargetIndex < targets.Count)
-            {
-                targets[currentTargetIndex].gameObject.SetActive(true);
-            }
-            else
-            {
-                waypointImage.enabled = false;
-                meter.text = "";
-            }
-        }
     }
 }

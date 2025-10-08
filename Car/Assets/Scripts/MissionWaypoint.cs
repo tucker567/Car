50b577f2daccd20f09b3de0f8ab5798c7489af92
tucker567 <tuckerstevens08@gmail.com>
2025-10-08 09:50:51 -0600

    Revert "Add multi-waypoint support to MissionWaypoint"
    This reverts commit d9c1837ddf9bccf6df44e4ed4541aa348d1d057d.

--- Car/Assets/Scripts/MissionWaypoint.cs
+++ Car/Assets/Scripts/MissionWaypoint.cs
@@ -2,39 +2,26 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class MissionWaypointManager : MonoBehaviour
+public class MissionWaypoint : MonoBehaviour
 {
-    public UnityEngine.Camera mainCamera;
     public Image waypointImage;
+    public Transform target;
     public TextMeshProUGUI meter;
     public Vector3 RectOffset;
-    public Transform player; // Assign your car/player transform here
-    public Transform[] targets;
-    public float switchDistance = 10f; // Distance to switch to next waypoint
-
-    private int currentTargetIndex = 0;
 
     void Update()
     {
-        if (targets == null || targets.Length == 0 || currentTargetIndex >= targets.Length)
-        {
-            waypointImage.enabled = false;
-            meter.enabled = false;
-            return;
-        }
-
-        Transform target = targets[currentTargetIndex];
-
         float minX = waypointImage.GetPixelAdjustedRect().width / 2;
         float maxX = Screen.width - minX;
 
         float minY = waypointImage.GetPixelAdjustedRect().height / 2;
         float maxY = Screen.height - minY;
 
-        Vector3 pos = mainCamera.WorldToScreenPoint(target.position + RectOffset);
+        Vector2 pos = UnityEngine.Camera.main.WorldToScreenPoint(target.position + RectOffset);
 
-        if (Vector3.Dot((target.position - player.position), player.forward) < 0)
+        if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
         {
+            // Target is behind the camera, clamp to nearest edge
             if (pos.x < Screen.width / 2)
                 pos.x = maxX;
             else
@@ -46,13 +33,7 @@ public class MissionWaypointManager : MonoBehaviour
 
         waypointImage.transform.position = pos;
 
-        float distance = Vector3.Distance(target.position, player.position);
+        float distance = Vector3.Distance(target.position, transform.position);
         meter.text = ((int)distance).ToString() + "m";
-
-        // Switch to next waypoint if close enough
-        if (distance < switchDistance)
-        {
-            currentTargetIndex++;
-        }
     }
 }
